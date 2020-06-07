dd41639cfe66cf36330df461104788cf0c21c253
YaromirKo <34893823+YaromirKo@users.noreply.github.com>
2020-06-08 02:50:51 +0500

    app vue-todo

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="app">
+      <h1>todo</h1>
+      <hr>
+      <router-view></router-view>
+  </div>
+</template>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <ul>
+            <todo-item
+                v-for="(todo, id) of todos" :key="id"
+                v-bind:todo="todo"
+                v-bind:id="id"
+                v-on:remove-todo="removeTodo"/>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import TodoItem from "@/components/TodoItem";
+    export default {
+        name: "TodoList",
+        props: ['todos'],
+        components: {
+            TodoItem,
+        },
+        methods: {
+            removeTodo(id) {
+                this.$emit('remove-todo', id)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+</style>
