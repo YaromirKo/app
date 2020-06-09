ba9e6a628c169908f6e95987ffb9c424ff5f4a56
YaromirKo <34893823+YaromirKo@users.noreply.github.com>
2020-06-09 14:26:28 +0500

    .

--- crm-finance/src/main.js
+++ crm-finance/src/main.js
@@ -14,17 +14,6 @@ import 'firebase/database'
 
 Vue.config.productionTip = false
 
-export default {
-  apiKey: 'AIzaSyCVrbYXNwDjM535tYPhJtmvYYksUMJldAA',
-  authDomain: 'crm-finance-vue.firebaseapp.com',
-  databaseURL: 'https://crm-finance-vue.firebaseio.com',
-  projectId: 'crm-finance-vue',
-  storageBucket: 'crm-finance-vue.appspot.com',
-  messagingSenderId: '107007426472',
-  appId: '1:107007426472:web:4220ce6e063fb8facf279d',
-  measurementId: 'G-Y7W9NCD0VL'
-}
-
 firebase.initializeApp(firebaseConfig)
 
 let app
